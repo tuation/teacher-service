495ea24b40aae897421e89f2c547a30d7b445e24
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 10:40:13 +0530

    added main and test class

---
+++ src/main/java/com/tuation/teacher/service/TeacherServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tuation.teacher.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuation/teacher/service/TeacherServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tuation.teacher.service;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
